32fdc8bc1d1a6eac58648cb78fec1a76693efc9b
Christian Schaefer <schaepp@gmx.de>
2018-11-17 06:18:42 +0100

    * Finalized structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">All</router-link>
-      <router-link to="/active">Active</router-link>
-      <router-link to="/completed">Completed</router-link>
+  <main id="app">
+    <header></header>
+    <div class="contents">
+      <nav id="nav">
+        <router-link to="/">All</router-link>
+        <router-link to="/active">Active</router-link>
+        <router-link to="/completed">Completed</router-link>
+      </nav>
+      <router-view/>
     </div>
-    <router-view/>
-  </div>
+  </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  --dark-brand-color: rgba(37, 76, 99, 1);
   --light-brand-color: rgba(108, 178, 206, 1);
+  --medium-brand-color: rgba(45, 156, 201, 1);
+  --dark-brand-color: rgba(37, 76, 99, 1);
+  --light-grey-color: rgba(249, 249, 249, 1);
+  --medium-grey-color: rgba(37, 76, 99, 1);
 
   * {
     font-family: "Lato", sans-serif;
--- src/views/Completed.vue
+++ src/views/Completed.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
     <TasksList v-bind:showCompletedStatus="true"/>
-    <FloatingButton/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TasksList from "@/components/TasksList.vue";
-import FloatingButton from "@/components/FloatingButton.vue";
 
 export default {
-  name: "home",
+  name: "completed",
   components: {
-    TasksList,
-    FloatingButton
+    TasksList
   }
 };
 </script>
